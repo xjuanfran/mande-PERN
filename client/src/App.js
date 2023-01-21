d1d413a73b124e8f947cbbbcc69b4d1d998c6bb0
juanDavidLEon <juan.garces.leon@correounivalle.edu.co>
2023-01-21 00:46:24 -0500

    Initial form login

--- client/src/App.js
+++ client/src/App.js
@@ -3,25 +3,25 @@ import Home from './components/Home'
 import PersonList from './components/PersonList'
 import PersonForm from './components/PersonForm'
 import Navbar from './components/Navbar'
-import {Container} from '@mui/material'
+//import {Container} from '@mui/material'
 import ClientForm from './components/ClientForm'
 import EmployeeForm from './components/EmployeeForm'
+import Login from './components/Login'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <Container>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/person" element={<PersonList />} />
         <Route path="/person/new" element={<PersonForm />} />
         <Route path="/client/:id/new" element={<ClientForm />} />
         <Route path="/employee/new" element={<EmployeeForm />} />
+        <Route path="/login" element={<Login />} />
         {/*mising route edit person*/}
         {/*mising route edit client*/}
       </Routes>
-      </Container>
     </BrowserRouter>
   )
 }
---
+++ client/src/components/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../style-sheet/Login.css'
+
+export default function Login() {
+  return (
+      <div className="main">
+        LOGIN
+      </div>    
+  )
+}
+
