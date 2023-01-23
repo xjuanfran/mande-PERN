dec927abbb862ba575513b5c92f0ae75d20a1dae
juanDavidLEon <juan.garces.leon@correounivalle.edu.co>
2023-01-22 23:13:42 -0500

    Home v1

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,8 +2,27 @@ import React from 'react'
 import { Button } from '@mui/material'
 import { HelmetProvider, Helmet } from 'react-helmet-async'
 import '../style-sheet/Home.css'
+import git from '../images/git.png'
+import instagram from '../images/instragramLogo.png'
+import facebook from '../images/faceLogo.png'
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
+
+  const [work, setWork] = useState([])
+
+  const loadWorks = async () => {
+    const response = await fetch("http://localhost:4000/work/active")
+    const data = await response.json()
+    console.log(data)
+    setWork(data)
+  }
+
+  useEffect(() => {
+    loadWorks()
+  }, [])
+
   return (
     <HelmetProvider>
       <Helmet>
@@ -17,7 +36,7 @@ export default function Home() {
           </p>
         </div>
         <div className="buttons">
-        <Button
+          <Button
             variant="contained"
             style={{
               backgroundColor: '#29323d',
@@ -39,7 +58,7 @@ export default function Home() {
               height: '3rem',
               alignItems: 'center',
             }}
-            sx={{ width: '10rem', textAlign: 'center', marginLeft: '1rem'}}
+            sx={{ width: '10rem', textAlign: 'center', marginLeft: '1rem' }}
             onClick={() => {
               window.location.href = '/login'
             }}
@@ -48,6 +67,28 @@ export default function Home() {
           </Button>
         </div>
       </div>
+      <div className="jobsSection">
+        <h1 className="titleJobs">Servicios ofrecidos en nuestra pagina</h1>
+        <div className='containerListW'>
+          {work.map((work) => (
+            <Link to="/person/new"><h2 key={work.work_id} className="worksList">{work.names}</h2></Link>
+          ))
+          }
+        </div>
+      </div>
+      <footer className="footer-distributed cssFooter">
+        <div className="footer-right">
+          <img src={git} alt="Logo gitHub" className='git' />
+          <img src={facebook} alt="Logo facebook" className='faceLogo' />
+          <img src={instagram} alt="Logo instragram" className='logoInstragram' />
+        </div>
+        <div className="footer-left">
+          <p className="footer-links">
+            <a className="link-1" href="/">Home</a>
+          </p>
+          <p>Mande &copy; 2023</p>
+        </div>
+      </footer>
     </HelmetProvider>
   )
 }
