358b443d19ad0c55fea8fc338a4fc5f840facec0
Santiago Vega <santiago.vega@correounivalle.edu.co>
2023-02-02 18:39:41 -0500

    Arreglo la barra del PersonForm

--- client/src/components/PersonForm.jsx
+++ client/src/components/PersonForm.jsx
@@ -147,13 +147,6 @@ export default function InputAdornments() {
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/login">Iniciar sesion</a>
-              </li>
-            </ul>
-          </div>
         </div>
       </nav>
       <Grid
